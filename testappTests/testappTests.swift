793dc31ba54b42bdc7dd501e691a6a26669563db
smiles0527 <iscurt.w@gmail.com>
2025-08-24 18:11:41 -0700

    Initial Commit

---
+++ testappTests/testappTests.swift
@@ -0,0 +1,16 @@
+//
+//  testappTests.swift
+//  testappTests
+//
+//  Created by Curtis Wei on 2025-08-24.
+//
+
+import Testing
+
+struct testappTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
